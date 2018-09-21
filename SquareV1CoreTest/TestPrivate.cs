09f1c0e1fd268426095040f8b35ddaa2fa1265d7
Daniel Przybylski <DanielP@meyer.com>
2018-09-21 13:53:39 -0700

    Drop file.

--- SquareV1CoreTest/TestPrivate.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Meyer.Square.V1.Test
-{
-    partial class Test
-    {
-        protected const string token = "sq0atp-BiDwJrmfSDvibmP5Ehv2tg";
-        protected const string location = "9A2EW9NG89Q9J";
-    }
-}
