a5681ff68b2400b39510621442aa1db7c88b01c3
Daniel Przybylski <DanielP@meyer.com>
2018-09-21 14:13:51 -0700

    Reset.

--- SquareV1CoreTest/Test.cs
+++ SquareV1CoreTest/Test.cs
@@ -3,5 +3,7 @@
     public partial class Test
     {
         protected const string baseurl = "https://connect.squareup.com/v1/";
+        //protected const string location = "";
+        //protected const string token = "";
     }
 }
