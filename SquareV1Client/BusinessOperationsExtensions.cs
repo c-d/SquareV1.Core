e79e8c758f2db2c698dc9147104971fdda61b186
Daniel Przybylski <DanielP@meyer.com>
2018-09-25 15:58:20 -0700

    Some employee support.

--- SquareV1Client/BusinessOperationsExtensions.cs
+++ SquareV1Client/BusinessOperationsExtensions.cs
@@ -10,7 +10,29 @@ namespace MeyerCorp.Square.V1
         /// <param name='operations'>
         /// The operations group for this extension method.
         /// </param>
-        public static Merchant Get(this IBusinessOperations operations, string locationId = null)
+        public static Merchant Get(this IBusinessOperations operations)
+        {
+            return Task.Factory.StartNew(s => ((IBusinessOperations)s).GetAsync(), operations, CancellationToken.None, TaskCreationOptions.None, TaskScheduler.Default).Unwrap().GetAwaiter().GetResult();
+        }
+
+        /// <param name='operations'>
+        /// The operations group for this extension method.
+        /// </param>
+        /// <param name='cancellationToken'>
+        /// The cancellation token.
+        /// </param>
+        public static async Task<Merchant> GetAsync(this IBusinessOperations operations,  CancellationToken cancellationToken = default(CancellationToken))
+        {
+            using (var _result = await operations.GetWithHttpMessagesAsync(null, cancellationToken).ConfigureAwait(false))
+            {
+                return _result.Body;
+            }
+        }
+
+        /// <param name='operations'>
+        /// The operations group for this extension method.
+        /// </param>
+        public static Merchant Get(this IBusinessOperations operations, string locationId )
         {
             return Task.Factory.StartNew(s => ((IBusinessOperations)s).GetAsync(locationId), operations, CancellationToken.None, TaskCreationOptions.None, TaskScheduler.Default).Unwrap().GetAwaiter().GetResult();
         }
@@ -21,7 +43,7 @@ namespace MeyerCorp.Square.V1
         /// <param name='cancellationToken'>
         /// The cancellation token.
         /// </param>
-        public static async Task<Merchant> GetAsync(this IBusinessOperations operations, string locationId = null, CancellationToken cancellationToken = default(CancellationToken))
+        public static async Task<Merchant> GetAsync(this IBusinessOperations operations, string locationId , CancellationToken cancellationToken = default(CancellationToken))
         {
             using (var _result = await operations.GetWithHttpMessagesAsync(locationId,null, cancellationToken).ConfigureAwait(false))
             {
--- SquareV1Client/IBusinessOperations.cs
+++ SquareV1Client/IBusinessOperations.cs
@@ -20,7 +20,15 @@ namespace MeyerCorp.Square.V1
         /// </param>
         Task<HttpOperationResponse<Merchant>> GetWithHttpMessagesAsync(string locationId = null, Dictionary<string, List<string>> customHeaders = null, CancellationToken cancellationToken = default(CancellationToken), Uri uri = null);
 
-        /// <param name='id'>
+         /// <param name='customHeaders'>
+        /// The headers that will be added to request.
+        /// </param>
+        /// <param name='cancellationToken'>
+        /// The cancellation token.
+        /// </param>
+        Task<HttpOperationResponse<Merchant>> GetWithHttpMessagesAsync(Dictionary<string, List<string>> customHeaders = null, CancellationToken cancellationToken = default(CancellationToken), Uri uri = null);
+
+       /// <param name='id'>
         /// </param>
         /// <param name='value'>
         /// </param>
