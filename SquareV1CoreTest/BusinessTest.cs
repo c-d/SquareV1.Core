2cf37e4b5634938f291c92e45e40371fe7f4c3d0
Daniel Przybylski <danielP@meyer.com>
2018-09-30 20:33:47 -0700

    Set up sync unit tests for businesses/locations.

--- SquareV1CoreTest/BusinessTest.cs
+++ SquareV1CoreTest/BusinessTest.cs
@@ -13,7 +13,7 @@ namespace Meyer.Square.V1.Test
         //const string location = "";
         //const string token = "";
 
-        [Fact(DisplayName = "Business: Get Me")]
+        [Fact(DisplayName = "Business: Get Me async")]
         public async Task GetTestAsync()
         {
             try
@@ -34,7 +34,7 @@ namespace Meyer.Square.V1.Test
             }
         }
 
-        [Fact(DisplayName = "Business: Get by Location ID")]
+        [Fact(DisplayName = "Business: Get by Location ID async")]
         public async Task GetSpecificTestAsync()
         {
             try
@@ -44,6 +44,50 @@ namespace Meyer.Square.V1.Test
                 using (var client = new Client(new Uri(baseurl), credentials))
                 {
                     var merchant = await client.BusinessOperations.GetAsync(location);
+
+                    Assert.Equal("C16C5851HEJR5", merchant.Id);
+
+                    System.Diagnostics.Debug.WriteLine(merchant.Id);
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex.Message);
+                throw;
+            }
+        }
+
+        [Fact(DisplayName = "Business: Get Me")]
+        public void GetTest()
+        {
+            try
+            {
+                var credentials = new TokenCredentials(token) as ServiceClientCredentials;
+
+                using (var client = new Client(new Uri(baseurl), credentials))
+                {
+                    var merchant = client.BusinessOperations.Get();
+
+                    System.Diagnostics.Debug.WriteLine(merchant);
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex.Message);
+                throw;
+            }
+        }
+
+        [Fact(DisplayName = "Business: Get by Location ID")]
+        public void GetSpecificTest()
+        {
+            try
+            {
+                var credentials = new TokenCredentials(token) as ServiceClientCredentials;
+
+                using (var client = new Client(new Uri(baseurl), credentials))
+                {
+                    var merchant = client.BusinessOperations.Get(location);
 
                     Assert.Equal("C16C5851HEJR5", merchant.Id);
 
